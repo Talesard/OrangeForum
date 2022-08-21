ae87f6e2c5104bb0ea6aa5589dedc7f08d49e276
Talesard <vetero4ekcs@mail.ru>
2022-08-22 02:29:05 +0300

    config2

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoUrl: 'mongodb://localhost:27017/2chdb',
+  appPort: 3000,
+  boardsList: ['b', 'gd', 'hw', 'mobi', 'pr', 'ra', 's', 's', 't', 'web', 'mochatest'],
+  imageExtensions: ['png', 'jpg', 'jpeg', 'webp', 'gif'],
+};
